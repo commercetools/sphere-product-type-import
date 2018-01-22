bba7d2ce11287e89a2a7ef83ea7ab1e6358157c3
Jan Juna <mail@janjuna.cz>
2018-01-22 20:37:18 +0100

    chore(indentation): Fix indentation according to linter

--- tests/integration/product-type-import.spec.js
+++ tests/integration/product-type-import.spec.js
@@ -48,119 +48,58 @@ const before = function setupSphereCreds () {
 
 test(`productType import module
   should not import product type without key`, (t) => {
-    t.timeoutAfter(10000)
-    const productType = {
-      name: 'custom-product-type',
-      description: 1244,
-      attributes: [],
-    }
-    before()
-      .then(() =>
-        productTypeImport.importProductType(productType))
-      .then(() => {
-        t.end('Error - should throw validation error')
-      })
-      .catch((err) => {
-        t.equal(
-          err.message.includes('Validation error on productType'
+  t.timeoutAfter(10000)
+  const productType = {
+    name: 'custom-product-type',
+    description: 1244,
+    attributes: [],
+  }
+  before()
+    .then(() =>
+      productTypeImport.importProductType(productType))
+    .then(() => {
+      t.end('Error - should throw validation error')
+    })
+    .catch((err) => {
+      t.equal(
+        err.message.includes('Validation error on productType'
             + ' "custom-product-type"'),
-          true,
-          'Importer should throw validation error',
-        )
+        true,
+        'Importer should throw validation error',
+      )
 
-        return client.productTypes.where(`name="${productType.name}"`).fetch()
-          .then(({ body: { results: productTypes } }) => {
-            const _actual = productTypes.length
+      return client.productTypes.where(`name="${productType.name}"`).fetch()
+        .then(({ body: { results: productTypes } }) => {
+          const _actual = productTypes.length
 
-            t.equal(_actual, 0, 'ProductTypes should not be imported')
-            t.end()
-          })
-      })
-  })
+          t.equal(_actual, 0, 'ProductTypes should not be imported')
+          t.end()
+        })
+    })
+})
 
 test(`productType import module
   should import a complete product type`, (t) => {
-    t.timeoutAfter(10000)
-    before().then(() => {
-      const productType = {
-        key: 'random-key',
-        name: 'custom-product-type',
-        description: 'Some cool description',
-        attributes: [
-          {
-            name: 'width',
-            label: {
-              de: 'Width',
-            },
-            type: {
-              name: 'number',
-            },
-            attributeConstraint: 'None',
-            isRequired: false,
-            isSearchable: false,
+  t.timeoutAfter(10000)
+  before().then(() => {
+    const productType = {
+      key: 'random-key',
+      name: 'custom-product-type',
+      description: 'Some cool description',
+      attributes: [
+        {
+          name: 'width',
+          label: {
+            de: 'Width',
           },
-          {
-            name: 'color',
-            label: {
-              de: 'Color',
-            },
-            type: {
-              name: 'ltext',
-            },
-            attributeConstraint: 'None',
-            isRequired: false,
-            isSearchable: false,
-            inputHint: 'SingleLine',
+          type: {
+            name: 'number',
           },
-        ],
-      }
-      productTypeImport.importProductType(productType)
-        .then(() => {
-          const summary = JSON.parse(productTypeImport.summaryReport())
-          const actual = summary.errors.length
-          const expected = 0
-
-          t.equal(actual, expected, 'There should be no error')
-          return client.productTypes.where(`name="${productType.name}"`).fetch()
-            .then(({ body: { results: productTypes } }) => {
-              const _actual = productTypes.length
-              const _expected = 1
-
-              t.equal(_actual, _expected, 'ProductTypes count should be same')
-
-              t.end()
-            })
-        })
-        .catch(t.end)
-    })
-      .catch(t.end)
-  })
-
-test(`productType import module
-  should add an attribute to an existing product type`, (t) => {
-    t.timeoutAfter(10000)
-    before().then(() => {
-      const productType = {
-        key: 'random-key',
-        name: 'custom-product-type',
-        description: 'Some cool description',
-        attributes: [
-          {
-            name: 'width',
-            label: {
-              de: 'Width',
-            },
-            type: {
-              name: 'number',
-            },
-            attributeConstraint: 'None',
-            isRequired: false,
-            isSearchable: false,
-          },
-        ],
-      }
-      const updatedProductType = Object.assign({}, productType, {
-        attributes: [{
+          attributeConstraint: 'None',
+          isRequired: false,
+          isSearchable: false,
+        },
+        {
           name: 'color',
           label: {
             de: 'Color',
@@ -172,41 +111,102 @@ test(`productType import module
           isRequired: false,
           isSearchable: false,
           inputHint: 'SingleLine',
-        }],
+        },
+      ],
+    }
+    productTypeImport.importProductType(productType)
+      .then(() => {
+        const summary = JSON.parse(productTypeImport.summaryReport())
+        const actual = summary.errors.length
+        const expected = 0
+
+        t.equal(actual, expected, 'There should be no error')
+        return client.productTypes.where(`name="${productType.name}"`).fetch()
+          .then(({ body: { results: productTypes } }) => {
+            const _actual = productTypes.length
+            const _expected = 1
+
+            t.equal(_actual, _expected, 'ProductTypes count should be same')
+
+            t.end()
+          })
       })
-      productTypeImport.importProductType(productType)
-        .then(() => {
-          const summary = JSON.parse(productTypeImport.summaryReport())
-          const actual = summary.errors.length
-          const expected = 0
-
-          t.equal(actual, expected, 'There should be no error')
-
-          return client.productTypes.where(`name="${productType.name}"`).fetch()
-        })
-        .then(({ body: { results: productTypes } }) => {
-          const actual = productTypes.length
-          const expected = 1
-
-          t.equal(actual, expected, 'ProductTypes count should be equal')
-        })
-        .then(() => productTypeImport.importProductType(updatedProductType))
-        .then(() => {
-          const summary = JSON.parse(productTypeImport.summaryReport())
-          const actual = summary.errors.length
-          const expected = 0
-          t.equal(actual, expected, 'There should be no error')
-
-          return client.productTypes.where(`name="${productType.name}"`).fetch()
-        })
-        .then(({ body: { results: productTypes } }) => {
-          const [importedProductType] = productTypes
-          t.equal(!!importedProductType, true, 'ProductType should be imported')
-          const actual = importedProductType.attributes.map(a => a.name)
-          const expected = ['width', 'color']
-          t.deepEqual(actual, expected, 'Product attributes should be equal')
-          t.end()
-        })
-    })
       .catch(t.end)
   })
+    .catch(t.end)
+})
+
+test(`productType import module
+  should add an attribute to an existing product type`, (t) => {
+  t.timeoutAfter(10000)
+  before().then(() => {
+    const productType = {
+      key: 'random-key',
+      name: 'custom-product-type',
+      description: 'Some cool description',
+      attributes: [
+        {
+          name: 'width',
+          label: {
+            de: 'Width',
+          },
+          type: {
+            name: 'number',
+          },
+          attributeConstraint: 'None',
+          isRequired: false,
+          isSearchable: false,
+        },
+      ],
+    }
+    const updatedProductType = Object.assign({}, productType, {
+      attributes: [{
+        name: 'color',
+        label: {
+          de: 'Color',
+        },
+        type: {
+          name: 'ltext',
+        },
+        attributeConstraint: 'None',
+        isRequired: false,
+        isSearchable: false,
+        inputHint: 'SingleLine',
+      }],
+    })
+    productTypeImport.importProductType(productType)
+      .then(() => {
+        const summary = JSON.parse(productTypeImport.summaryReport())
+        const actual = summary.errors.length
+        const expected = 0
+
+        t.equal(actual, expected, 'There should be no error')
+
+        return client.productTypes.where(`name="${productType.name}"`).fetch()
+      })
+      .then(({ body: { results: productTypes } }) => {
+        const actual = productTypes.length
+        const expected = 1
+
+        t.equal(actual, expected, 'ProductTypes count should be equal')
+      })
+      .then(() => productTypeImport.importProductType(updatedProductType))
+      .then(() => {
+        const summary = JSON.parse(productTypeImport.summaryReport())
+        const actual = summary.errors.length
+        const expected = 0
+        t.equal(actual, expected, 'There should be no error')
+
+        return client.productTypes.where(`name="${productType.name}"`).fetch()
+      })
+      .then(({ body: { results: productTypes } }) => {
+        const [importedProductType] = productTypes
+        t.equal(!!importedProductType, true, 'ProductType should be imported')
+        const actual = importedProductType.attributes.map(a => a.name)
+        const expected = ['width', 'color']
+        t.deepEqual(actual, expected, 'Product attributes should be equal')
+        t.end()
+      })
+  })
+    .catch(t.end)
+})
